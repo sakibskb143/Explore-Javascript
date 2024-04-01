abbe983aeb7bffeb85b56cf911d84e321e84ce0b
sakibskb143 <sakibskb143@gmail.com>
2024-04-01 12:40:31 +0600

    function JS file intial commit

---
+++ jS.Function/function.js
@@ -0,0 +1,24 @@
+// function syntax
+// function name (){
+
+    // function statement 
+
+// }
+function learnJava(){
+    console.log("Try to clear basic concept .");
+    console.log("I Know you visit my github.");
+}
+// function call 
+learnJava();
+
+const price = 25 ;
+function youCanBuy (price){
+    if(price >= 25){
+        console.log("you can buy chocolate , icecream , biscuit, chips etc");
+    }
+    else {
+        console.log("You cannot buy  anythings.");
+    }
+  
+}
+youCanBuy(price);
